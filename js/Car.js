c3c5b47723d0cedc2ed595ea8d92aa5d974adb9e
payelbera <60781680+payelbera@users.noreply.github.com>
2022-09-29 16:30:58 +0530

    Update Car.js

--- js/Car.js
+++ js/Car.js
@@ -5,9 +5,6 @@ AFRAME.registerComponent("car-model", {
   init: function() {
     // Do something when component first attached.
     this.el.setAttribute("gltf-model", this.data.modelRef);
-    const position = { x: 0, y: 50, z: 80 };
-    const rotation = { x: 0, y: -100, z: 0 };
-    this.el.setAttribute("position", position);
-    this.el.setAttribute("rotation", rotation);
+    //declare position and rotation and set it as attribute
   }
 });
